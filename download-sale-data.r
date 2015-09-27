c4f2ca667402e5b2f20a77216acf96197f2b1e5a
Rob Barry <rob.barry@gmail.com>
2015-09-27 14:54:47 -0400

    fixes and extend years

--- download-sale-data.r
+++ download-sale-data.r
@@ -5,11 +5,15 @@ source("setup.r")
 
 download.sale.data <- function(data.files) {
   for (i in 1:nrow(data.files)) {
-    record <- raw.files[i, ]
+    record <- data.files[i, ]
     local.file <- paste0(dest.path, record$local.filename)
-    if (!file.exists(local.file)) 
+    if (!file.exists(local.file)) {
+      print(record$remote.filename)
       download.file(record$remote.filename, destfile = local.file, method = "curl")    
+    }
+      
   }
 }
 
 download.sale.data(data.files)
+
--- setup.r
+++ setup.r
@@ -10,7 +10,7 @@ dest.path <- "data/dof/downloads/"
 setwd(paste(repos, "nyc-property", sep = "/"))
 
 boros <- c("queens", "brooklyn", "bronx", "manhattan", "si")
-years <- 2014:2015
+years <- 2004:2015
 
 data.files <- expand.grid(years, boros, stringsAsFactors = F)
 colnames(data.files) <- c("year", "boro")
@@ -25,18 +25,40 @@ data.files$use.year <-
 # AND because of the Department of Finance...
 data.files$use.boro <- ifelse(data.files$year > "2013" & data.files$boro == "si", "statenisland", data.files$boro)
 
-# And also because these guys!
+# And also because these guys! They change the URL like every year...
 data.files$remote.filename <-
-  apply(data.files, 1, function(x) {
-    if (x["year"] == "2014") {
-      return(
-        paste0("http://www1.nyc.gov/assets/finance/downloads/pdf/rolling_sales/annualized-sales/",
-               x["year"], "/", x["year"], "_", x["use.boro"], ".xls")
-      )
-    } else if (x["year"] == "2015") {
-      paste0("http://www1.nyc.gov/assets/finance/downloads/pdf/rolling_sales/rollingsales_",
-             x["use.boro"], ".xls")
-    }
-  })
+  apply(data.files,
+        1,
+        function(x) {
+          if (x["year"] %in% as.character(2004:2006)) {
+            # 2004-2006 format: http://www1.nyc.gov/assets/finance/downloads/sales_manhattan_06.xls
+            paste0("http://www1.nyc.gov/assets/finance/downloads/sales_",
+                   x["use.boro"], "_", x["use.year"], ".xls")
+          } else if (x["year"] == "2007") {
+            # 2007 format: http://www1.nyc.gov/assets/finance/downloads/excel/rolling_sales/sales_2007_manhattan.xls
+            paste0("http://www1.nyc.gov/assets/finance/downloads/excel/rolling_sales/sales_",
+                   x["use.year"], "_", x["use.boro"], ".xls")
+          } else if (x["year"] == "2008") {
+            # 2008 format: http://www1.nyc.gov/assets/finance/downloads/pdf/09pdf/rolling_sales/sales_2008_manhattan.xls
+            paste0("http://www1.nyc.gov/assets/finance/downloads/pdf/09pdf/rolling_sales/sales_",
+                   x["use.year"], "_", x["use.boro"], ".xls")
+          } else if (x["year"] == "2009") {
+            # 2009 format: http://www1.nyc.gov/assets/finance/downloads/pdf/rolling_sales/annualized-sales/2009_manhattan.xls
+            paste0("http://www1.nyc.gov/assets/finance/downloads/pdf/rolling_sales/annualized-sales/",
+                   x["use.year"], "_", x["use.boro"], ".xls")
+            
+          } else if (x["year"] %in% as.character(2010:2014)) {
+            # 2010-2014 format: http://www1.nyc.gov/assets/finance/downloads/pdf/rolling_sales/annualized-sales/2014/2014_manhattan.xls
+            paste0("http://www1.nyc.gov/assets/finance/downloads/pdf/rolling_sales/annualized-sales/",
+                   x["year"], "/", x["year"], "_", x["use.boro"], ".xls")
+            
+          } else if (x["year"] == "2015") {
+            # Rolling format: 
+            paste0("http://www1.nyc.gov/assets/finance/downloads/pdf/rolling_sales/rollingsales_",
+                   x["use.boro"], ".xls")
+          } else {
+            NA
+          }
+        })
 
 data.files$local.filename <- paste0(data.files$year, "-", data.files$boro, ".xls")
